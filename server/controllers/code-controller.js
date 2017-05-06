089bb3e523ee2f8da8f7f016021944d15cd6b6e8
DiyanaDimitrova <H_E_R_A_92@abv.bg>
2017-05-06 15:33:40 +0300

    send mail and code

--- server/controllers/code-controller.js
+++ server/controllers/code-controller.js
@@ -1,6 +1,36 @@
 let Event = require('mongoose').model('Event')
 let Code = require('mongoose').model('Code')
 let User = require('mongoose').model('User')
+let email = require('../utilities/email')
+let crypto = require('crypto')
+let algorithm = 'aes-256-ctr'
+let password = 'solarstone'
+
+let encrypt = (text) => {
+  let cipher = crypto.createCipher(algorithm, password)
+  let crypted = cipher.update(text, 'utf8', 'hex')
+  crypted += cipher.final('hex')
+  return crypted
+}
+let decrypt = (text) => {
+  let decipher = crypto.createDecipher(algorithm, password)
+  let dec = decipher.update(text, 'hex', 'utf8')
+  dec += decipher.final('utf8')
+  let obj = JSON.parse(dec)
+  return obj
+}
+let createCryptedObj = (req, obj) => {
+  let cryptString = {
+    'firstName': obj.firstName,
+    'lastName': obj.lastName,
+    'email': obj.email
+  }
+  let hw = encrypt(JSON.stringify(cryptString))
+  // console.log('HW' + hw)
+  return hw
+  // req.session.cryptString = hw
+  // console.log('encrypted string created !')
+}
 
 module.exports = {
   listInvited: (req, res) => {
@@ -16,50 +46,61 @@ module.exports = {
   },
   sendCode: (req, res) => {
     console.log(req.body)
-    User
-    .find({ username: { $in: req.body.usersToSendCode } })
-    .then((users) => {
-      console.log(users)
-    })
-    .catch((err) => {
-      console.log(err)
-      res.json({message: err})
-    })
-  },
-  create: (req, res) => {
-    let code = req.body
-    Code
-      .create(code)
-      .then(code => {
-        res.json({message: 'OK'})
-      })
-      .catch(err => {
-        res.json({message: err})
-      })
-  },
-  changeReservedStatus: (req, res) => {
-    let id = req.params.id
-    Code
-      .findById(id)
-      .then(code => {
-        Code
-          .findByIdAndUpdate(id, {
-            $set: {isReserved: !code.isReserved}
-          })
-          .exec()
-          .then(() => {
-            res.json({message: 'OK'})
-          })
-          .catch((err) => {
-            console.log(err)
-            res.json({message: err})
-          })
-      })
-      .catch((err) => {
-        console.log(err)
-        res.json({message: err})
-      })
+    Event
+     .findById(req.body.eventId)
+     .then((event) => {
+       User
+       .find({ username: { $in: req.body.usersToSendCode } })
+       .then((users) => {
+         users.forEach(user => {
+          //  console.log('USER' + JSON.stringify(user))
+           email.sendTicket(createCryptedObj(req, user), user, event)
+         })
+       })
+       .catch((err) => {
+         console.log(err)
+         res.json({message: err})
+       })
+     })
+     .catch((err) => {
+       console.log(err)
+       res.json({message: err})
+     })
   }
+  // create: (req, res) => {
+  //   let code = req.body
+  //   Code
+  //     .create(code)
+  //     .then(code => {
+  //       res.json({message: 'OK'})
+  //     })
+  //     .catch(err => {
+  //       res.json({message: err})
+  //     })
+  // },
+  // changeReservedStatus: (req, res) => {
+  //   let id = req.params.id
+  //   Code
+  //     .findById(id)
+  //     .then(code => {
+  //       Code
+  //         .findByIdAndUpdate(id, {
+  //           $set: {isReserved: !code.isReserved}
+  //         })
+  //         .exec()
+  //         .then(() => {
+  //           res.json({message: 'OK'})
+  //         })
+  //         .catch((err) => {
+  //           console.log(err)
+  //           res.json({message: err})
+  //         })
+  //     })
+  //     .catch((err) => {
+  //       console.log(err)
+  //       res.json({message: err})
+  //     })
+  // }
   // avaible: (req, res) => {
   //   let allCodes = {}
   //   Code
---
+++ server/utilities/email.js
@@ -0,0 +1,65 @@
+let nodemailer = require('nodemailer')
+let sgTransport = require('nodemailer-sendgrid-transport')
+let pdf = require('html-pdf')
+let qr = require('qr-image')
+require('dotenv').config()
+let dateFormat = require('dateFormat')
+
+let options = {
+  auth: {
+    api_key: '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'
+  }
+}
+
+let mailer = nodemailer.createTransport(sgTransport(options))
+let optionsPDF = {
+  'format': 'A4',
+  'orientation': 'portrait'
+}
+module.exports = {
+  sendTicket: (cryptedString, user, event) => {
+    //  console.log('EVENT ' + JSON.stringify(event))
+    let qrSvg = qr.imageSync(cryptedString, { type: 'svg' })
+    let date = dateFormat(event.time, 'HH:MM') + ' ' + dateFormat(event.date, 'dddd, mmmm dS, yyyy')
+    let artistName = event.title
+    let clubName = event.place
+    let guestEmail = user.email
+    let name = user.firstName + ' ' + user.lastName
+    // let address = 'Sofia, bul. Maria Luiza 126'
+    let emailContent = '<b>Здравей </b>' + name + '<br><p> Благодарим ти за интереса към нашето събитие.' +
+                          'Като прикачен файл ще откриеш билет като комплимент от нас.<br> Надяваме се да ти хареса!</p>' +
+                          '<p>Научи повече за нас и как осъществяваме събитията си <a href="http://dgty-promo.com/faq/"> тук </a>.</p>' +
+                          '<p>Екипа на Tranc3motion.</p>'
+    let htmlTicket = '<html><head><meta charset="utf8"></head><body style="">' +
+                        '<div style="overflow: hidden; font-family: Arial, Helvetica, sans-serif; border-bottom-style: dashed;">' +
+                        '<div style="float:left; width:65%; padding: 0 0 0 20px;">' +
+                        '<h3>' + artistName + '</h3>' +
+                        '<p>Name: ' + name + '<br> <br> Date: ' + date + '</p>' +
+                        '<p><b>Place: ' + clubName + '</b>' + '</p>' +
+                        '</div><div style="max-width: 30%; height:180px; width:100%; float:right;">' + qrSvg + '</div></div>' +
+                        '</body></html>'
+
+    pdf.create(htmlTicket, optionsPDF).toBuffer(function (err, buffer) {
+      if (err) console.log(err)
+      let email = {
+        to: [guestEmail],
+        from: 'testAccount@gmail.com',
+        subject: 'Test ticket',
+        text: 'Test ticket',
+        html: emailContent,
+        attachments: [{
+          filename: 'Ticket.pdf',
+          content: buffer,
+          contentType: 'application/pdf'
+        }]
+      }
+      // console.log('TICKET' + JSON.stringify(email))
+      mailer.sendMail(email, function (err, res) {
+        if (err) {
+          console.log('ERR MAIL' + JSON.stringify(err))
+        }
+        console.log('RES' + JSON.stringify(res))
+      })
+    })
+  }
+}
