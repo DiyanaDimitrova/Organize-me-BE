3b1eb11fe062fb79acbf7060bbc6a2535022440f
DiyanaDimitrova <H_E_R_A_92@abv.bg>
2017-09-10 13:35:19 +0300

    fix

--- server/config/config.js
+++ server/config/config.js
@@ -11,7 +11,7 @@ module.exports = {
   },
   production: {
     rootPath: rootPath,
-    db: process.env.MONGO_DB_CONN_STRING,
+    db: 'mongodb://didi:didi@ds113841.mlab.com:13841/organize-me',
     port: process.env.port
   }
 }
